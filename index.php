c508be3f0499b24925811e3c04b5eb5dd5651fdb
Alexsmobsenyojer <85582400+Alexsmobsenyojer@users.noreply.github.com>
2021-11-03 11:16:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ class Bagetomat
     public int $productCount;
     public int $productPrice;
     public int $insertedCoins = 0;
-    public int $returnCoins;
+    public int $returnCoins = 0;
     public int $bank;
 
     public function constructor__(int $productCount,int $productPrice,int $bank)
@@ -38,7 +38,7 @@ class Bagetomat
      {
         $this->returnCoins = $this->insertedCoins - $this->productPrice;
          
-         if($this->bank > $this->returnCoins ){echo "tady je vaše bageta +". $this->returnCoins. "kč";}
+         if($this->bank > $this->returnCoins ){echo "tady je vaše bageta +". $this->returnCoins. "kč"; $this->bank-=$this->returnCoins;}
          else {echo "nemame na vraceni".$this->returnCoins ."kč SMŮLA :)";}
      }
      else {echo "tady je vaše bageta";} 
@@ -51,7 +51,6 @@ try
 {$purchase1->insertCoin(60);}
 catch (\Throwable $th) {
     echo  $th->getMessage();}
-
 ?>
 </body>
 </html>
